d5f258602675dbd956053eccec4129fa07737c28
Avinna <avinna123@gmail.com>
2021-05-14 04:40:03 -0500

    using arrays

--- src/Arrays.java
+++ src/Arrays.java
@@ -1,7 +1,24 @@
 public class Arrays {
 
     public static void main(String[] args) {
+    int[][] arr= new int[5][5];
+    int val=1;
 
+    for(int row=0;row<5;row++){
+        System.out.println();
+        for (int col=0;col<row+1; col++){
+            arr[row][col]= val;
+            val++;
+        }
+    }
+    for (int i=0;i<5;i++){
+        for (int j=0;j<5;j++) {
+            if (arr[i][j] > 0)
+            System.out.print(arr[i][j] + " ");
+        }
+        System.out.println();
+
+    }
     }
 
 }
